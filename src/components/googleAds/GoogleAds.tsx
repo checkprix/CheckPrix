148e6bf35758bdd2b795c0e38190534d63d28e21
shubhamjatara <bhadoriya.shubham1995@gmail.com>
2024-03-15 16:54:45 +0530

    working on adsense

--- src/components/googleAds/GoogleAds.tsx
+++ src/components/googleAds/GoogleAds.tsx
@@ -1,22 +1,42 @@
 import { useEffect } from "react";
-
+import { useSearchParams } from "react-router-dom";
 const GoogleAds = () => {
+
+  const useSearch = useSearchParams();
   useEffect(() => {
-    if ((window as any).adsbygoogle) {
-        (window as any).adsbygoogle = (window as any).adsbygoogle || [];
-        (window as any).adsbygoogle.push({});
-    }
-  }, []);
+    const scriptElement = window.document.querySelector(
+      'script[src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3961225425063983"]'
+    );
+    
+    const handlescriptLoad = () => {
+      try {
+        if ((window as any).adsbygoogle) {
+           (window as any).adsbygoogle = (window as any).adsbygoogle || [];
+          (window as any).adsbygoogle.push({});
+        } else {
+          scriptElement!.addEventListener("load", handlescriptLoad);
+          console.log("waiting unitl adsense lib is loaded");
+        }
+      } catch (err) {
+        console.log("err in adsence", err);
+      }
+    };
+
+    handlescriptLoad();
+  }, [useSearch]);
 
   return (
     <>
-      <ins
-        className="adsbygoogle"
-        style={{ display: "block" }}
-        data-ad-client="pub-3961225425063983"
-        data-ad-slot="f08c47fec0942fa0"
-        data-ad-format="auto"
-      />
+      <div className="overflow-hidden m-5">
+        <ins
+          className="adsbygoogle"
+          style={{ display: "block" }}
+          data-ad-client="pub-3961225425063983"
+          data-ad-slot="f08c47fec0942fa0"
+          data-ad-format="auto"
+          data-full-widdth-responsive="true"
+        />
+      </div>
     </>
   );
 };
